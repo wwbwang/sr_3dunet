176649951a5740ede5ce240823f05f1ab8f095dd
wangwb <wwbwang99@gamil.com>
2023-12-20 17:50:00 +0800

    inference-2d-cyclegan

---
+++ inference_cyclegan.sh
@@ -0,0 +1,8 @@
+
+branch_name="projection_cyclegan"
+iter='100'
+CUDA_VISIBLE_DEVICES=2 python scripts/inference_projection_cyclegan.py \
+    --expname "${branch_name}_net_g_${iter}" --num_io_consumer 1 --aniso_dimension -1\
+    -i /home/wangwb/workspace/sr_3dunet/datasets/rotated_blocks/val\
+    -o /home/wangwb/workspace/sr_3dunet/results/${branch_name}_net_g_${iter}\
+    --model_path /home/wangwb/workspace/sr_3dunet/experiments/${branch_name}/models/net_g_A_${iter}.pth\
